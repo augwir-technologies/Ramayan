d1a4c236bd8ec1cbbbedc946cbdfd2221ef590f1
aditek91 <aditya.augwir@gmail.com>
2019-04-26 19:42:58 +0530

    Ramayan Terrain
    - Terrain
    - FPS
    - Instantiate
    - Spawning
    - LookRot

---
+++ Ramayan/Assets/Spawning.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawning : MonoBehaviour
+{
+    public GameObject Enemies;
+    GameObject[] SpawnEnemies;
+    int index;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            SpawnEnemies = GameObject.FindGameObjectsWithTag("Respawn");
+            index = Random.Range(0, SpawnEnemies.Length);
+            Instantiate(Enemies, SpawnEnemies[index].transform.position, SpawnEnemies[index].transform.rotation);
+        }   
+    }
+}
---
+++ Ramayan/Assets/ZombieAttack.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieAttack : MonoBehaviour
+{
+    public GameObject Player;
+    Vector3 offset;
+    float movespeed = 1f;
+    float rotatespeed = 10f;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.position = Vector3.MoveTowards(transform.position, Player.transform.position, movespeed * Time.deltaTime );
+        offset = Player.transform.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(offset, Vector3.up * rotatespeed * Time.deltaTime);
+        transform.rotation = rotation;
+
+        float distance = Vector3.Distance(Player.transform.position, transform.position);
+        if (distance < 2)
+        {
+            movespeed = 0;
+            Debug.Log(movespeed + "this is the move speed");
+            anim.SetBool("Attack", true);
+        }
+        else
+        {
+            anim.SetBool("Attack", false);
+        }
+    }
+}
