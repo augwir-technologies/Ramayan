903d4900c1352c338c0dad6e706b93b1fdfdd87b
uday <uday.bojja@augwir.com>
2019-04-26 14:08:14 +0530

    Destructible wall tweak01

---
+++ Ramayan/Assets/Ramayan/Scripts/DestructibleScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructibleScript : MonoBehaviour
+{
+    public float minForce;
+    public float maxForce;
+    public float radius;
+
+    public void Destruction()
+    {
+        foreach(Transform t in transform)
+        {
+            var rb = t.GetComponent<Rigidbody>();
+            if(rb!= null)
+            {
+                rb.AddExplosionForce(Random.Range(minForce, maxForce), transform.position, radius);
+            }
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Ramayan/Assets/Ramayan/Scripts/SpawnDestructible.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnDestructible : MonoBehaviour
+{
+    public GameObject originalPrefab;
+    public GameObject destructiblePrefab;
+     
+    public void SpawnDestructiblePrefab()
+    {
+        originalPrefab.SetActive(false);
+        destructiblePrefab.SetActive(true);
+        destructiblePrefab.GetComponent<DestructibleScript>().Destruction();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalPrefab.SetActive(true);
+        destructiblePrefab.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SpawnDestructiblePrefab();
+        }
+    }
+}
