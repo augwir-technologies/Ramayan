ae08c77fec0a1b452509b8b1d8c1553674e029ff
uday <uday.bojja@augwir.com>
2019-04-26 15:00:12 +0530

    Destructible Wall Tweak02

--- Ramayan/Assets/Ramayan/Scripts/SpawnDestructible.cs
+++ Ramayan/Assets/Ramayan/Scripts/SpawnDestructible.cs
@@ -6,18 +6,42 @@ public class SpawnDestructible : MonoBehaviour
 {
     public GameObject originalPrefab;
     public GameObject destructiblePrefab;
-     
-    public void SpawnDestructiblePrefab()
+    public GameObject firePrefab;
+    public GameObject smokePrefab;
+    public GameObject dustPrefab;
+
+
+    IEnumerator SpawnDestructiblePrefab()
     {
+        yield return new WaitForSeconds(10);
         originalPrefab.SetActive(false);
         destructiblePrefab.SetActive(true);
+        firePrefab.SetActive(true);
+        smokePrefab.SetActive(true);
+        dustPrefab.SetActive(true);
         destructiblePrefab.GetComponent<DestructibleScript>().Destruction();
+
+        yield return null;
     }
     // Start is called before the first frame update
     void Start()
     {
         originalPrefab.SetActive(true);
         destructiblePrefab.SetActive(false);
+        firePrefab.SetActive(false);
+        smokePrefab.SetActive(false);
+        dustPrefab.SetActive(false);
+    }
+
+    public void SpawnFireEffect()
+    {
+        originalPrefab.SetActive(true);
+        destructiblePrefab.SetActive(false);
+        firePrefab.SetActive(true);
+        smokePrefab.SetActive(true);
+        dustPrefab.SetActive(false);
+
+        StartCoroutine("SpawnDestructiblePrefab");
     }
 
     // Update is called once per frame
@@ -25,7 +49,7 @@ public class SpawnDestructible : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            SpawnDestructiblePrefab();
+            SpawnFireEffect();
         }
     }
 }
